b7bfb2e6f8b62031ecd3a3f6740fe9dcedd44477
eronzg <eiriogbe@gmail.com>
2020-12-20 00:18:25 +0100

    version 2

--- BMI_calc_pkg/bmi_calc.py
+++ BMI_calc_pkg/bmi_calc.py
@@ -1,24 +1,4 @@
-import decimal
-
-
-def float_range(start, stop, step):
-  while start < stop:
-    yield float(start)
-    start = decimal.Decimal(start)
-    start += decimal.Decimal(step)
-
-# print(list(float_range(0, 1, '0.1')))
-
-
 class BMI:
-    STATUS = {
-        'Underweight': float_range(0, 18.5, '0.1'),
-        'Normal weight': float_range(18.5, 24.9, '0.1'),
-        'Overweight': float_range(25.0, 29.9, '0.1'),
-        'Obesity class I': float_range(30.0, 34.9, '0.1'),
-        'Obesity class II': float_range(35.0, 39.9, '0.1'),
-    }
-
     def __init__(self, mass, height):
         self.mass = mass
         self.height = height
@@ -27,15 +7,30 @@ class BMI:
         return str(self.check())
 
     def check(self):
-        """Calculates the body mass index"""
+        """
+        Calculates the body mass index
+        height in metres and mass in kg
+        """
         return round(self.mass/(self.height**2), 1)
 
     def report(self):
-        for k, v in self.STATUS.items():
-            if self.check() in v:
-                return k
-            elif self.check() > 40:
-                return 'Obesity class III'
-        return "Invalid mass or height input."
+        if self.check() > 0 and self.check() < 18.5:
+            return 'Underweight'
+        elif self.check() > 18.5 and self.check() < 24.9:
+            return 'Normal weight'
+        elif self.check() > 25.0 and self.check() < 29.9:
+            return 'Overweight'
+        elif self.check() > 30.0 and self.check() < 34.9:
+            return 'Obesity class I'
+        elif self.check() > 35.0 and self.check() < 39.9:
+            return 'Obesity class II'
+        elif self.check() > 40:
+            return 'Obesity class III'
+        else:
+            return 'Invalid mass or height input.'
 
+
+"""uncomment the following code to test"""
+# eroms = BMI(75, 1.83) 
+# print(eroms.report())
 
